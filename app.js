a910b1341fb107f44309cb713611520221689ae6
thousif <thousif20@gmail.com>
2017-05-24 09:18:52 +0530

    updating turtle position

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ var dots = [];
 var img = new Image();
 
 img.onload = function() {
-	ctx.drawImage(img, 0, 0);
-    ctx.beginPath();
-    ctx.moveTo(30, 96);
-    ctx.lineTo(70, 66);
-    ctx.lineTo(103, 76);
-    ctx.lineTo(170, 15);
-    ctx.stroke();
+	ctx.drawImage(img, 0, 450 ,50,50);
 }
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 img.src = "turtle.png";
 
@@ -41,8 +38,6 @@ drawDots = function(){
 	    	dots[c][r].x = dotX;
 	    	dots[c][r].y = dotY;
 
-	    	console.log(dotX,dotY);
-
 	    	ctx.beginPath();
 			ctx.arc(dotX,dotY, 5, 0, Math.PI*2, false);
 			ctx.fillStyle = "green";
@@ -51,7 +46,64 @@ drawDots = function(){
 
 	    }
 	}
-
 }
 
+var TO_RADIANS = Math.PI/180; 
+function drawRotatedImage(image, x, y, angle) { 
+ 
+	// save the current co-ordinate system 
+	// before we screw with it
+	ctx.save(); 
+ 
+	// move to the middle of where we want to draw our image
+	ctx.translate(x, y);
+ 
+	// rotate around that point, converting our 
+	// angle from degrees to radians 
+	ctx.rotate(angle * TO_RADIANS);
+ 
+	// draw it up and to the left by half the width
+	// and height of the image 
+	ctx.drawImage(image, -(image.width/2), -(image.height/2),50,50);
+ 
+	// and restore the co-ords to how they were when we began
+	ctx.restore(); 
+}
+
+var left = function(){
+	drawRotatedImage(img,canvas.width,canvas.height,90);
+}
+
+function keyDownHandler(e) {
+    switch(e.keyCode){
+    	case 37 :
+    		console.log("37");
+    		left();
+    	    break;
+    	case 38 :
+    		console.log("38");
+    		up();
+    	    break;
+    	case 39 :
+    		console.log("39");
+    		right();
+    	    break;
+    	case 40 :
+    		console.log("40");
+    		down();
+    	    break; 
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+
 drawDots();
+
